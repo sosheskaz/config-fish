08925b1bce68ced15cb154e0db0f9190cdf3fbc2
Eric Miller <1588827+sosheskaz@users.noreply.github.com>
2025-12-18 14:52:19 -0500

    fix(greeting): filter only to avail cows

--- functions/fish_greeting.fish
+++ functions/fish_greeting.fish
@@ -1,8 +1,19 @@
 function fish_greeting
-  set -l fortune_cmd "$(command -v fortune)"
+  set -f fortune_cmd "$(command -v fortune)"
   if test -z "$fortune_cmd"; return; end
 
-  set -l cowfile "$(random choice daemon dragon eyes fox stegosaurus sus turtle)"
+  begin
+    set -l cowsay_all_choices daemon dragon eyes fox stegosaurus sus turtle
+    set -l avail_cows (string split ' ' (command cowsay -l | tail -n +2))
+    for cow in $cowsay_all_choices
+      if contains -- $cow $avail_cows
+        set -fa cows $cow
+      else
+      end
+    end
+  end
+
+  set -f cowfile "$(random choice $cows)"
 
   command $fortune_cmd | cowsay -f "$cowfile" | lolcat
 end
