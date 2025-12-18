2ec5b47d998280a4a3e4ac29a71a8df101a4d2b6
Eric Miller <1588827+sosheskaz@users.noreply.github.com>
2025-12-18 14:36:38 -0500

    fix(lolcat): better handle case where lolcat not found

--- conf.d/98-lolcat.fish
+++ conf.d/98-lolcat.fish
@@ -1,11 +1,11 @@
 function lolcat
-  set -l lolcat_cmd "$(command -v lolcat)"
+  set -f lolcat_cmd "$(command -v lolcat)"
   if test $status = 0
     if test "$COLORTERM" = "truecolor" -o "$COLORTERM" = "24bit"
-      set -la lolcat_cmd -t
+      set -fa lolcat_cmd -t
     end
   else
-    set -l lolcat_cmd "$(command -v cat)"
+    set -f lolcat_cmd cat
   end
 
   command $lolcat_cmd $argv
