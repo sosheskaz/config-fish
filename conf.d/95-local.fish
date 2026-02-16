42d56d3d5b8360cd285ffe8d23bcc59cdc182a90
Eric Miller <1588827+sosheskaz@users.noreply.github.com>
2026-02-16 12:41:44 -0600

    fix: prepend local paths instead of append, only if present

--- conf.d/95-local.fish
+++ conf.d/95-local.fish
@@ -1,1 +1,6 @@
-fish_add_path "$HOME/.local/bin" "$HOME/.local/sbin"
+if test -d "$HOME/.local/sbin"
+    fish_add_path -p "$HOME/.local/sbin"
+end
+if test -d "$HOME/.local/bin"
+    fish_add_path -p "$HOME/.local/bin"
+end
