3e416faf901b1057d38aad4af3ae5cf4a1873b90
Eric Miller <1588827+sosheskaz@users.noreply.github.com>
2026-02-26 23:01:16 -0600

    fix: add my goproxy

--- conf.d/80-go.fish
+++ conf.d/80-go.fish
@@ -1,6 +1,7 @@
 set -qx GOPATH      || set -gx GOPATH "$HOME/go"
 set -qx CGO_ENABLED || set -gx CGO_ENABLED 0
 set -qx GOPRIVATE   || set -gx GOPRIVATE github.com/sosheskaz
+set -qx GOPROXY     || set -gx GOPROXY https://goproxy.miller-machine.xyz,direct
 
 if test -d "$GOPATH/bin"
   fish_add_path -a "$GOPATH/bin" # add it to the path, but low-priority.
