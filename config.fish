bd6f260b9d61624f264085bd4a9c1f3d42db5b10
Eric Miller <1588827+sosheskaz@users.noreply.github.com>
2026-05-16 10:29:38 -0500

    fix: document write-locking config.fish

--- config.fish
+++ config.fish
@@ -3,3 +3,8 @@ if status is-interactive
     # conf.d/ files auto-load at startup
     # functions/ lazy-load on first use
 end
+
+# Is stuff getting added below by over-eager integrations that you don't want?
+# chmod a-w ~/.config/fish/config.fish
+# Revert with
+# chmod u+w ~/.config/fish/config.fish
